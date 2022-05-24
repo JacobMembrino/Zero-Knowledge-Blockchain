21111d08761ab959bb6335a4c9476d078b7d9519
Jacob Membrino <jacob.membrino@gmail.com>
2022-05-24 16:21:56 -0400

    Update Blockchain.py

--- Blockchain.py
+++ Blockchain.py
@@ -9,7 +9,7 @@ class Transaction:
     self.payer = payer
     self.payee = payee
    def toString:
-    return 'Amount:', str(amount), 'Payer:', str(payer), 'Payee:', str(payee)
+    return str('Amount:', str(amount), 'Payer:', str(payer), 'Payee:', str(payee))
   
 class Block:
   def __int__(self, lastHash, transaction, timestamp):
@@ -24,16 +24,81 @@ class Block:
     return hash.digest('hex')
 
 class Chain:
-  
+  # Singleton instance
+  public static instance = new Chain()
+  chain: Block[]
+
+  def __init__(self, chain):
+    this.chain = [
+      # Genesis block
+      new Block('', new Transaction(100, 'genesis', 'satoshi')) ]
+
+  # Most recent block
+  get lastBlock():
+    return this.chain[this.chain.length - 1]
+
+  # Proof of work system
+  mine(nonce: number):
+    let solution = 1;
+    console.log('⛏️  mining...')
+
+    while(true):
+
+      const hash = crypto.createHash('MD5')
+      hash.update((nonce + solution).toString()).end()
+
+      const attempt = hash.digest('hex')
+
+      if(attempt.substr(0,4) === '0000'):
+        console.log(`Solved: ${solution}`)
+        return solution;
+      
+
+      solution += 1;
+
+  #Add a new block to the chain if valid signature & proof of work is complete
+  addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer):
+    const verify = crypto.createVerify('SHA256')
+    verify.update(transaction.toString())
+
+    const isValid = verify.verify(senderPublicKey, signature)
+
+    if (isValid):
+      const newBlock = new Block(this.lastBlock.hash, transaction)
+      this.mine(newBlock.nonce)
+      this.chain.push(newBlock)
+
 class Wallet:
-  
-  
-const satoshi = new Wallet();
-const bob = new Wallet();
-const alice = new Wallet();
+  public publicKey: string
+  public privateKey: string
 
-satoshi.sendMoney(50, bob.publicKey);
-bob.sendMoney(23, alice.publicKey);
-alice.sendMoney(5, bob.publicKey);
+  constructor() 
+    const keypair = crypto.generateKeyPairSync('rsa', {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: 'spki', format: 'pem' },
+      privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+    })
 
+    this.privateKey = keypair.privateKey
+    this.publicKey = keypair.publicKey
+  
+
+  sendMoney(amount: number, payeePublicKey: string):
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey)
+
+    const sign = crypto.createSign('SHA256')
+    sign.update(transaction.toString()).end()
+
+    const signature = sign.sign(this.privateKey)
+    Chain.instance.addBlock(transaction, this.publicKey, signature)
+
+
+# WALLET INSTANCES
+const satoshi = new Wallet()
+const bob = new Wallet()
+const alice = new Wallet()
+
+satoshi.sendMoney(50, bob.publicKey)
+bob.sendMoney(23, alice.publicKey)
+alice.sendMoney(5, bob.publicKey)
 console.log(Chain.instance)
