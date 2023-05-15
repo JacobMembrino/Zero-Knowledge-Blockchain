483ac2d2fece35d21b17e60953a1c9b8bb5f18f0
Jacob Membrino <jacob.membrino@gmail.com>
2023-05-15 13:02:26 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    // Create a new blockchain
+    let mut blockchain = Blockchain::new();
+
+    // Add sample transactions and mine a new block
+    let transactions = vec![
+        Transaction::new(String::from("Alice"), String::from("Bob"), 100),
+        Transaction::new(String::from("Bob"), String::from("Alice"), 50),
+    ];
+    blockchain.add_block(transactions);
+
+    // Add more transactions and mine another block
+    let transactions = vec![
+        Transaction::new(String::from("Alice"), String::from("Charlie"), 200),
+        Transaction::new(String::from("Charlie"), String::from("Bob"), 75),
+    ];
+    blockchain.add_block(transactions);
+
+    // Display the current state of the blockchain
+    for block in blockchain.blocks.iter() {
+        println!("{:?}", block);
+    }
 }
